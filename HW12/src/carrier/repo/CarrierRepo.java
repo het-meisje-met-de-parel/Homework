3002b2acbef00a2caa3913a776eaa48c7875921c
Cara <43150098+het-meisje-met-de-parel@users.noreply.github.com>
2019-12-29 11:51:47 +0300

    HW 12. Базовая версия

---
+++ HW12/src/carrier/repo/CarrierRepo.java
@@ -0,0 +1,12 @@
+package carrier.repo;
+
+import carrier.domain.Carrier;
+import common.business.repo.CommonRepo;
+
+public interface CarrierRepo extends CommonRepo<Carrier, Long> {
+
+  Carrier getByIdFetchingTransportations(long id);
+
+  Carrier[] findByName(String name);
+
+}
---
+++ HW12/src/storage/initor/InitStorageType.java
@@ -0,0 +1,5 @@
+package storage.initor;
+
+public enum InitStorageType {
+  MEMORY, TEXT_FILE
+}
